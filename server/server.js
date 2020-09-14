4d32863d4210a4926b2c8d5139dfe9bcee956630
akina ohira <akina568325@gmail.com>
2020-09-14 23:50:21 +0900

    changed path for build staic assets

--- server/server.js
+++ server/server.js
@@ -4,13 +4,10 @@ const path = require("path");
 const db = require("./knex.js");
 
 //set up static assets
-app.use(express.static(path.resolve(__dirname, "build")));
-
+// __dirname --> directory name of file is
+app.use(express.static(path.resolve(__dirname, "..", "build")));
 // listing port that setting in .env or default 4000
 const port = process.env.PORT || 4000;
-app.listen(port, () => {
-  console.log(`App listing port ${port}`);
-});
 
 // Api to get data from backend
 app.get("/api/allshoes", async (req, res) => {
@@ -21,4 +18,8 @@ app.get("/api/allshoes", async (req, res) => {
     console.error("Error loading locations!", err);
     res.sendStatus(500);
   }
+});
+
+app.listen(port, () => {
+  console.log(`App listing port ${port}`);
 });
